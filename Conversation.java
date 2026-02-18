4a479ff1ab5cf3e7393c9d626803d3caa97eee9b
amberzhanglc <lzhang35@smith.edu>
2026-02-17 19:49:37 -0500

    Amber_A3

--- Conversation.java
+++ Conversation.java
@@ -2,44 +2,175 @@
 // only modify the body of each function
 class Conversation implements ConversationRequirements {
 
-  // Attributes 
+  // Attributes
+
+  private java.util.Scanner scanner;
+  private java.util.Random random;
+
+  private int rounds;
+
+  private String[] transcript;
+  private int transcriptIndex;
+
+  private static String[] cannedResponses = {
+    "Mmm-hm.",
+    "Tell me more.",
+    "I see.",
+    "Interesting."
+  };
+
+  private String[][] mirrorWords = {
+    {"i", "you"},
+    {"me", "you"},
+    {"am", "are"},
+    {"you", "i"},
+    {"my", "your"},
+    {"your", "my"}
+  };
+
 
   /**
-   * Constructor 
+   * Constructor
    */
   Conversation() {
-    
+
+    scanner = new java.util.Scanner(System.in);
+    random = new java.util.Random();
+
+    transcriptIndex = 0;
   }
+
 
   /**
    * Starts and runs the conversation with the user
    */
   public void chat() {
 
+    // ask rounds
+    System.out.print("How many rounds?  ");
+    rounds = scanner.nextInt();
+    scanner.nextLine();
+
+    // create transcript array
+    transcript = new String[2 * rounds + 2];
+
+    // greeting
+    String greeting = "Hi there!  What's on your mind?";
+    System.out.println(greeting);
+
+    transcript[transcriptIndex] = greeting;
+    transcriptIndex++;
+
+
+    // conversation loop
+    for (int i = 0; i < rounds; i++) {
+
+      String userInput = scanner.nextLine();
+
+      transcript[transcriptIndex] = userInput;
+      transcriptIndex++;
+
+      String response = respond(userInput);
+
+      System.out.println(response);
+
+      transcript[transcriptIndex] = response;
+      transcriptIndex++;
+    }
+
+
+    // goodbye
+    String goodbye = "See ya!";
+    System.out.println(goodbye);
+
+    transcript[transcriptIndex] = goodbye;
+    transcriptIndex++;
   }
+
 
   /**
    * Prints transcript of conversation
    */
   public void printTranscript() {
 
+    System.out.println();
+    System.out.println("TRANSCRIPT:");
+
+    for (int i = 0; i < transcriptIndex; i++) {
+
+      System.out.println(transcript[i]);
+    }
   }
 
+
   /**
-   * Gives appropriate response (mirrored or canned) to user input
-   * @param inputString the users last line of input
-   * @return mirrored or canned response to user input  
+   * Gives appropriate response
    */
   public String respond(String inputString) {
-    String returnString = ""; 
-    return returnString; 
+
+    String lower = inputString.toLowerCase();
+
+    String[] words = lower.split(" ");
+
+    boolean foundMirror = false;
+
+
+    // check mirror words
+    for (int i = 0; i < words.length; i++) {
+
+      for (int j = 0; j < mirrorWords.length; j++) {
+
+        if (words[i].equals(mirrorWords[j][0])) {
+
+          foundMirror = true;
+        }
+      }
+    }
+
+
+    // if mirror found
+    if (foundMirror) {
+
+      String[] newWords = new String[words.length];
+
+      for (int i = 0; i < words.length; i++) {
+
+        boolean replaced = false;
+
+        for (int j = 0; j < mirrorWords.length; j++) {
+
+          if (words[i].equals(mirrorWords[j][0])) {
+
+            newWords[i] = mirrorWords[j][1];
+            replaced = true;
+          }
+        }
+
+        if (replaced == false) {
+
+          newWords[i] = words[i];
+        }
+      }
+
+      String result = String.join(" ", newWords);
+
+      return result + "?";
+    }
+
+
+    // otherwise canned response
+    int index = random.nextInt(cannedResponses.length);
+
+    return cannedResponses[index];
   }
+
 
   public static void main(String[] arguments) {
 
     Conversation myConversation = new Conversation();
-    myConversation.chat();
-    myConversation.printTranscript();
 
+    myConversation.chat();
+
+    myConversation.printTranscript();
   }
 }
--- reflection.md
+++ reflection.md
@@ -1,7 +1,17 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+    Yuli Qin, Olivia Huang
+
 - Which session(s) of TA / office hours did you attend?
+    Feb 17th tutor drop-in hours (7 pm - 9 pm)
+
 - What are your initial impressions of Java? 
+    It's a stricter language and we need to write it in a very standard way. It is more complicated than Python.
+
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+    Similarities: Both language support object-oriented programming.
+    Differences: Java only support object-oriented programming. Java is more organized. The syntax are different for the two languages.
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+    The program runs well although I felt a little confused at first. My advice is that we should have a general sense of the logic of the program we want to write and break it down to write it step by step. In addition, tutoring hours are also really helpful, so do not hesitate to seek for help.
